87f46b3adbdca70ff4f210d6aae985b05bc9c8be
Nash3r221 <braeyoung@gmail.com>
2025-07-10 23:39:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const cors_proxy = require('cors-anywhere');
 
-const cors_proxy = require("cors-anywhere");
+const app = express();
 
 const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
   originWhitelist: [], // allow all origins
-  requireHeader: ['origin', 'x-requested-with', 'apikey'], // required headers
-  removeHeaders: ['cookie', 'cookie2']
-}).listen(port, host, function () {
-  console.log('Running CORS Anywhere on ' + host + ':' + port);
+  requireHeader: ['origin', 'x-requested-with', 'apikey'],
+  removeHeaders: ['cookie', 'cookie2'],
+}).listen(port, host, () => {
+  console.log(`CORS Anywhere running on ${host}:${port}`);
 });
 
 app.use('/', (req, res) => {
